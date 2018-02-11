0a350099053ba4f9092b0c7091276df96b6d4223
Rajath P Javali <rajathjavali.com>
2018-02-11 11:06:19 -0700

    adding color to pichart

--- script.js
+++ script.js
@@ -41,6 +41,8 @@ function analyzeData(target, lines)
 			if(i[0] === "#")
 			continue;
 			let line = i.split(' ', 8);
+			if(line[6] == undefined)
+				continue;
 			if(data[line[6]])
 				data[line[6]]++;
 			else
@@ -48,23 +50,42 @@ function analyzeData(target, lines)
 		}
 
 		for (let i in data){
-			if(i != "undefined")
-			{
+			//if(i != "undefined")
+			//{
 				let string = i;
 				string = i[0].toUpperCase();
 				string += i.substring(1, i.length);
 				final[k++] = {"key": string, "count": data[i]};
-			}
+			//}
 		}
 		
 		final.sort(function(a, b) {
 			return parseInt(b.count) - parseInt(a.count);
 		})
 
+		let limiter = 25;
+		let num = document.getElementById('number').value;
+		if(num)
+			limiter = num > final.length ? final.length : num;
+
+		if(final.length > limiter)
+		{
+			let trimFinal = [];
+			for(let i = 0; i < limiter; i++)
+			{
+				trimFinal[i] = Object.assign({}, final[i]);
+			}
+			trimFinal[limiter] = {"key": "Others", "count": 0};
+			for(let i = limiter; i < final.length; i++)
+			{
+				trimFinal[limiter].count += final[i].count;
+			}
+			final = trimFinal;
+		}
 		let table = new Table()
 		table.createTable(final)
 		let chart = new PiChart()
-		chart.createChart(final)
+		chart.createChart2(final)
 	}
 }
 
@@ -103,8 +124,8 @@ class PiChart
 		let width = this.chart.node().getBoundingClientRect().width;
 		let height = this.chart.node().getBoundingClientRect().height;
 		this.outerRadius = Math.min(width, height) * 0.5 - 40;
-		this.radius = Math.min(width, height) / 2,
-		this.chart.append("g").attr("class", "piChart").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+		this.radius = Math.min(width, height) / 2 - 20;
+		this.chart.append("g").attr("class", "piChart").attr("transform", "translate(" + (width / 2 + 10) + "," + height / 2 + ")");
 
 	}
 
@@ -152,4 +173,95 @@ class PiChart
             });
 
 	}
+
+	createChart2(data)
+	{
+		let _this = this;
+		let index = 0;
+		let radiusLabel = _this.radius - _this.radius / 4;
+
+		//let color = d3.scale.category20(); 
+		function colorPicker(d) {
+		  var colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+		  return colors[d % colors.length];
+		}
+
+		let path = d3.arc().innerRadius(_this.radius / 8).outerRadius(_this.radius - _this.radius / 4);
+		let label = d3.arc().innerRadius(radiusLabel).outerRadius(radiusLabel);
+		let pie = d3.pie().value(function(d) {
+		  return d.count;
+		})
+		.sort(null)
+		.startAngle(-10 * (Math.PI/180))
+        .endAngle(350 * (Math.PI/180));
+
+		function midAngle(d){
+			return d.startAngle + (d.endAngle - d.startAngle)/2;
+		}
+
+		let arc =  	this.chart.select(".piChart").selectAll(".arc").data(pie(data));
+		let newArc = arc.enter().append("g");
+
+		arc.exit().remove();
+		arc = newArc.merge(arc);
+		arc.attr("class", "arc")
+			.on("mouseover", function(e) {
+				d3.selectAll(".arc").attr("style", "opacity: 0.3;");
+				d3.select(this).style("opacity", 1).append("title").text("Page Views: " + e.data.count);
+			})
+			.on("mouseout", function(e) {
+				d3.selectAll(".arc").attr("style", "opacity: 1");
+				d3.select(this).select("title").remove();
+			});
+
+		let pathArc = arc.selectAll("path").data(function(d){return d3.select(this).data();});
+		let newPath = pathArc.enter().append("path");
+
+		pathArc.exit().remove();
+		pathArc = newPath.merge(pathArc);
+
+		pathArc.attr("d", path)
+			.attr("fill", function(d) {	return colorPicker(index++); });
+
+		let text = arc.selectAll("text").data(function(d){return d3.select(this).data();});
+		let newText = text.enter().append("text");
+
+		text.exit().remove();
+		text = newText.merge(text);
+
+		text.text(function(d) {
+			  return d.data.key;
+			})
+			.style("text-anchor", function(d) {
+			  return midAngle(d) > Math.PI? "end": "start";
+			})
+			.attr("d", label)
+			.attr("transform", function(d) {
+					var position = label.centroid(d);
+					if(midAngle(d) > Math.PI)
+						position[0] -= 40
+					else
+						position[0] +=  40 //path.centroid(d)//midAngle(d)//radiusLabel * (midAngle(d) > Math.PI? -1: 1) - 2;
+					position[1] += 2;
+					console.log(position);
+					return "translate(" + position + ")";
+			 });
+			
+
+		let polyline = arc.selectAll("polyline").data(function(d){return d3.select(this).data();});
+		let newline = polyline.enter().append("polyline");
+
+		polyline.exit().remove();
+		polyline = newline.merge(polyline);
+
+		polyline.attr("points", function(d) {
+				    let position = label.centroid(d);
+				    if(midAngle(d) > Math.PI)
+						position[0] -= 40
+					else
+				   		position[0] += 40// radiusLabel * (midAngle(d)> Math.PI? -1: 1);
+				    return [path.centroid(d), label.centroid(d), position];
+				});
+	};
+
 }
